a09f83f5f1ac326c3112f6ea54410a5c6099f259
david <simplesam8856@gmail.com>
2021-06-29 14:59:29 +1000

    a little bit of coding

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,16 @@ CREATE DATABASE fruit_tree_finder;
 CREATE TABLE fruit_trees (
   id SERIAL PRIMARY KEY,
   tree_type TEXT,
-  -- how to store locs,
-  user TEXT,
-)
+  loc_lat FLOAT,
+  loc_long FLOAT,
+  details TEXT,
+  user INT,
+);
+
+CREATE TABLE user (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  email TEXT,
+  password TEXT,
+);
 
