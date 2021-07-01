7adff642d3f225415491db306548bfde593110cd
david <simplesam8856@gmail.com>
2021-07-01 22:37:39 +1000

    heroku fix

--- client/js/document_dec.js
+++ client/js/document_dec.js
@@ -5,7 +5,8 @@ var fruitTreeLocations = [];
 var comments = [];
 var likes = [];
 var fruitTreeTypes = [];
-var userCoords = {lat: 0, lng: 0}
+var userCoords = { lat: 0, lng: 0 };
+var markers = [];
 
 const mapDiv = document.querySelector('#map');
 const fruitTreeSelectMap = document.querySelector('.fruit-tree-select-map');
@@ -33,36 +34,18 @@ const createFruitTreeContainer = document.querySelector(
   '.create-fruit-tree-container'
 );
 
-const addFruitForm = document.querySelector('.add-fruit-form')
+const addFruitForm = document.querySelector('.add-fruit-form');
 const fruitTreeSelect = document.querySelector('.fruit-tree-select');
 const fruitDetails = document.querySelector('#fruit-details');
 const fruitTreeSubmit = document.querySelector('.fruit-tree-submit');
-const userLat = document.querySelector('.user-lat')
-const userLng = document.querySelector('.user-lng')
+const userLat = document.querySelector('.user-lat');
+const userLng = document.querySelector('.user-lng');
 
-// const fruitTreeTypes = [
-//   'apple',
-//   'orange',
-//   'lemon',
-//   'mango',
-//   'grapes',
-//   'lime',
-//   'pomegranate',
-// ];
-var markers = [];
-let markerProps = [
-  {
-    coords: { lat: -37.8066381, lng: 144.98555159999998 },
-    iconImage:
-      'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-    content: 'apple',
-  },
-  {
-    coords: { lat: -38.3333, lng: 144.3167 },
-    content: 'mango',
-  },
-  {
-    coords: { lat: -38.4899, lng: 145.2038 },
-    content: 'apple',
-  }
-];
+function initMap() {
+  let mapOptions = {
+    center: { lat: -37.8136, lng: 144.9631 },
+    zoom: 8,
+  };
+
+  map = new google.maps.Map(mapDiv, mapOptions);
+}
--- models/comment.js
+++ models/comment.js
@@ -1,10 +1,17 @@
 const { Pool } = require('pg');
 
-const db = new Pool({
-  database: process.env.DATABASE_NAME,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASS,
-});
+let db;
+if (process.env.PRODUCTION) {
+  db = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+} else {
+  db = new Pool({
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+  });
+}
 
 const Comment = {
   findAll() {
