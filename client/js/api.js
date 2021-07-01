6b120fc57b5ad200e9a6c839afcbdc7d797e88ed
david <simplesam8856@gmail.com>
2021-07-01 17:11:42 +1000

    frontend refactor

--- client/js/api.js
+++ client/js/api.js
@@ -1,26 +1,21 @@
-let fruitTreeLocations = [];
-let comments = [];
-let likes = [];
-let fruitTreeTypes = [];
-
 const getFruitTrees = async () => {
   let res = await axios.get('/api/trees');
-  fruitTreeLocations = res.data;
+  return res.data;
 };
 
 const getComments = async () => {
   res = await axios.get('/api/comments');
-  comments = res.data;
+  return res.data;
 };
 
 const getLikes = async () => {
   res = await axios.get('/api/likes');
-  likes = res.data;
+  return res.data;
 };
 
 const getTypes = async () => {
   res = await axios.get('/api/types');
-  fruitTreeTypesTest = res.data;
+  return res.data;
 };
 
 const testReq = async () => {
@@ -34,8 +29,3 @@ const getUser = async (id) => {
   res = await axios.get(`/api/user/${id}`);
   console.log(res.data);
 };
-
-getFruitTrees();
-getComments();
-getLikes();
-getTypes();
--- client/js/page_builder.js
+++ client/js/page_builder.js
@@ -1,48 +1,80 @@
-
-// build the page that the user sees once the page has loaded. 
- 
-const fruitTreeListDiv = document.querySelector(".fruit-tree-list-container")
-const fruitList = document.querySelector(".fruit-tree-list")
-
-const createFruitTreeContainer = document.querySelector('.create-fruit-tree-container')
-const fruitTreeSelect = document.querySelector('.fruit-tree-select')
-const fruitDetails = document.querySelector('#fruit-details')
-const fruitTreeSubmit = document.querySelector('.fruit-tree-submit')
-
+// build the page that the user sees once the page has loaded.
 
 function createFruitDropDownMap() {
-    fruitTreeTypes.forEach(item => {
-        let fruitOption = document.createElement('option');
-        fruitOption.textContent = item;
-        fruitTreeSelectMap.appendChild(fruitOption);
-    })
+  fruitTreeTypes.forEach((item) => {
+    let fruitOption = document.createElement('option');
+    fruitOption.textContent = item;
+    fruitTreeSelectMap.appendChild(fruitOption);
+  });
 }
-
 
 function createFruitList(fruits) {
+  while (fruitList.firstChild) {
+    fruitList.firstChild.remove();
+  }
 
-    while (fruitList.firstChild) {
-        fruitList.firstChild.remove();
-    }
+  fruits.forEach((fruit) => {
+    let fruitItem = document.createElement('li');
+    fruitItem.textContent = `Fruit: ${fruit.content}`;
+    fruitList.appendChild(fruitItem);
+  });
 
-    fruits.forEach(fruit => {
-        let fruitItem = document.createElement("li")
-        fruitItem.textContent = `Fruit: ${fruit.content}`
-        fruitList.appendChild(fruitItem)
-    });
-
-    fruitTreeListDiv.appendChild(fruitList)
+  fruitTreeListDiv.appendChild(fruitList);
 }
-
 
 function addFruitTreeDiv() {
-    fruitTreeTypes.forEach(item => {
-        let fruitOption = document.createElement('option');
-        fruitOption.textContent = item;
-        fruitTreeSelect.appendChild(fruitOption);
-      })
+  fruitTreeTypes.forEach((item) => {
+    let fruitOption = document.createElement('option');
+    fruitOption.textContent = item;
+    fruitTreeSelect.appendChild(fruitOption);
+  });
 }
 
-createFruitDropDownMap()
+function buildMapMakers() {
+  fruitTreeLocations.forEach((marker) => {
+    addMarker(marker);
+  });
+}
 
-addFruitTreeDiv()
+function addMarker(props) {
+  let marker = new google.maps.Marker({
+    position: props.coords,
+    map: map,
+  });
+  if (props.iconImage) {
+    marker.setIcon(props.iconImage);
+  }
+
+  if (props.content) {
+    let infoWindow = new google.maps.InfoWindow({
+      content: props.content,
+    });
+
+    marker.addListener('click', function () {
+      infoWindow.open(map, marker);
+    });
+  }
+  markers.push(marker);
+}
+
+function initMap() {
+  let mapOptions = {
+    center: { lat: -37.8136, lng: 144.9631 },
+    zoom: 8,
+  };
+
+  map = new google.maps.Map(mapDiv, mapOptions);
+}
+
+const buildPage = async () => {
+  fruitTreeLocations = await getFruitTrees();
+  comments = await getComments();
+  likes = await getLikes();
+  fruitTreeTypes = await getTypes();
+
+  buildMapMakers();
+  createFruitDropDownMap();
+  addFruitTreeDiv();
+};
+
+buildPage();
