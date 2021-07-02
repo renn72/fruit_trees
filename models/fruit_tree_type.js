7f6eb51812be7878b1ad8c5e11d18cea367f6161
mchippendale <m.chippendale18@gmail.com>
2021-07-02 14:25:15 +1000

    marker icons set individual fruit type

--- models/fruit_tree_type.js
+++ models/fruit_tree_type.js
@@ -3,19 +3,25 @@ const FruitTreeType = {
     'cherry',
     'apple',
     'pear',
-    'peach',
-    'pineapple',
-    'walnut',
-    'lemon',
-    'orange',
-    'lime',
-    'mango',
-    'grape',
-    'plum',
-    'raspberry',
-    'pomegranate',
-    'banana',
   ],
 };
 
 module.exports = FruitTreeType;
+
+
+
+  // 'cherry',
+  // 'apple',
+  // 'pear',
+  // 'peach',
+  // 'pineapple',
+  // 'walnut',
+  // 'lemon',
+  // 'orange',
+  // 'lime',
+  // 'mango',
+  // 'grape',
+  // 'plum',
+  // 'raspberry',
+  // 'pomegranate',
+  // 'banana',
