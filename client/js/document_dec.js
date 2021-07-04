c0c5e01adffbc24d1134af16850765b76d56f901
david <simplesam8856@gmail.com>
2021-07-05 00:09:20 +1000

    user valid fixes

--- client/js/document_dec.js
+++ client/js/document_dec.js
@@ -8,9 +8,11 @@ var fruitTreeTypes = [];
 var userCoords = { lat: 0, lng: 0 };
 var markers = [];
 var infoObj = [];
-var loggedIn = false;
-var userId = 0;
-var userName = '';
+if (typeof (loggedIn !== 'undefined')) {
+  var loggedIn = false;
+}
+var userId;
+var userName;
 
 var treeIconMap = [
   {
@@ -60,19 +62,6 @@ const createUserForm = document.querySelector('.signup-form');
 const loginForm = document.querySelector('.login-form');
 
 const userThumbnailAccount = document.querySelector('.user-thumbnail-account');
-
-const userThumbnailInnerHtmlSVG = `<svg
-xmlns="http://www.w3.org/2000/svg"
-width="1em"
-height="1em"
-fill="currentColor"
-class="bi bi-person"
-viewBox="0 0 16 16"
->
-<path
-  d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
-/>
-</svg>`;
 
 function initMap() {
   let mapOptions = {
